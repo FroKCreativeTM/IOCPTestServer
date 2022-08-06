2e6e50b11e87433a2090251c49141c5a639b8e7a
seungCheolCha <39144582+FroKCreativeTM@users.noreply.github.com>
2022-08-06 12:00:28 +0900

    서버 컨텐츠 작업중

--- GameServer/Contents/Object/GameObject.cpp
+++ GameServer/Contents/Object/GameObject.cpp
@@ -3,6 +3,14 @@
 
 namespace FrokEngine
 {
+	GameObject::GameObject()
+	{
+	}
+
+	GameObject::~GameObject()
+	{
+	}
+
 	int GameObject::GetObjectId() const
 	{
 		return this->_objectId;
@@ -13,6 +21,69 @@ namespace FrokEngine
 		this->_objectId = id;
 	}
 
+	Protocol::StatInfo GameObject::GetStat()
+	{
+		return Protocol::StatInfo();
+	}
+
+	void GameObject::SetStat(const Protocol::StatInfo& stat)
+	{
+	}
+
+	Protocol::PositionInfo GameObject::GetPosInfo()
+	{
+		return Protocol::PositionInfo();
+	}
+
+	void GameObject::SetPosInfo(const Protocol::PositionInfo& posInfo)
+	{
+	}
+
+	Protocol::GameObjectType GameObject::GetGameObjectType() const
+	{
+		return Protocol::GameObjectType();
+	}
+
+	void GameObject::SetGameObjectType(const Protocol::GameObjectType& objectType)
+	{
+	}
+
+	Protocol::ObjectInfo GameObject::GetObjectInfo()
+	{
+		return Protocol::ObjectInfo();
+	}
+
+	void GameObject::SetObjectInfo(const Protocol::ObjectInfo& objInfo)
+	{
+	}
+
+	Protocol::CreatureState GameObject::GetCreatureState() const
+	{
+		return Protocol::CreatureState();
+	}
+
+	void GameObject::SetCreatureState(const Protocol::CreatureState& state)
+	{
+	}
+
+	float GameObject::GetSpeed() const
+	{
+		return 0.0f;
+	}
+
+	void GameObject::SetSpeed(float speed)
+	{
+	}
+
+	int32 GameObject::GetHP() const
+	{
+		return int32();
+	}
+
+	void GameObject::SetHP(int32 hp)
+	{
+	}
+
 	GameSessionRef GameObject::GetOwnerSession()
 	{
 		return this->_ownerSession;
@@ -21,5 +92,13 @@ namespace FrokEngine
 	void GameObject::SetOwnerSession(GameSessionRef ref)
 	{
 		this->_ownerSession = ref;
+	}
+
+	void GameObject::OnDamaged(GameObjectPtr attacker, int damage)
+	{
+	}
+
+	void GameObject::OnDead(GameObjectPtr attacker)
+	{
 	}
 }
--- GameServer/Contents/Object/GameObject.h
+++ GameServer/Contents/Object/GameObject.h
@@ -4,15 +4,53 @@ namespace FrokEngine
 {
 	class GameObject
 	{
-	public : 
+	public:
+		GameObject();
+		~GameObject();
+
+	public:
 		int GetObjectId() const;
 		void SetObjectId(int id);
+
+		Protocol::StatInfo GetStat();
+		void SetStat(const Protocol::StatInfo& stat);
+
+		Protocol::PositionInfo GetPosInfo();
+		void SetPosInfo(const Protocol::PositionInfo& posInfo);
+
+		Protocol::GameObjectType GetGameObjectType() const;
+		void SetGameObjectType(const Protocol::GameObjectType& objectType);
+
+		Protocol::ObjectInfo GetObjectInfo();
+		void SetObjectInfo(const Protocol::ObjectInfo& objInfo);
+
+		Protocol::CreatureState GetCreatureState() const;
+		void SetCreatureState(const Protocol::CreatureState& state);
+
+		float GetSpeed() const;
+		void SetSpeed(float speed);
+
+		int32 GetHP() const;
+		void SetHP(int32 hp);
 
 		GameSessionRef GetOwnerSession();
 		void SetOwnerSession(GameSessionRef ref);
 
+		virtual void OnDamaged(GameObjectPtr attacker, int damage);
+		virtual void OnDead(GameObjectPtr attacker);
+
 	protected : 
 		int _objectId;
+
+		// 오브젝트에 대한 정보가 담겨있음
+		Protocol::ObjectInfo			_objInfo;
+		Protocol::StatInfo				_statInfo;
+		Protocol::GameObjectType		_objectType;
+
+		float							_speed;
+		int32							_hp;
+		Protocol::CreatureState			_state;
+
 		GameSessionRef			_ownerSession; // Cycle
 
 		Protocol::GameObjectType _objectType = Protocol::GameObjectType::GAME_OBJECT_TYPE_NONE;
